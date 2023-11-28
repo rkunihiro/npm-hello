948310ce208e27d3dec683d4bc6446ad8f05d927
Ryoh Kunihiro <11649545+rkunihiro@users.noreply.github.com>
2023-11-28 21:23:01 +0900

    initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    printWidth: 80,
+    // tabWidth: 2, // from .editorconfig
+    // useTabs: false, // from .editorconfig
+    semi: true,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    trailingComma: "all",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always",
+    quoteProps: "as-needed",
+};
